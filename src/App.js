7e9437de8cef3c4fe3d8d54fc394482b478db4f6
MUHAMMAD CASDI <KNO2104136@smsfinance.co.id>
2023-07-08 00:01:44 +0700

    Added Vercel logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import vercel from './vercel.png';
 import './App.css';
 
 function App() {
@@ -6,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={vercel} className="App-logo" alt="logo" />
       </header>
     </div>
   );
